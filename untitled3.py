41377e282bec8e7e88685e8912942089a6001c01
maker565 <oceanoantarticoagua@gmail.com>
2026-02-24 13:58:54 -0300

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,13 @@
+'''for i in range(1,10):
+    print(i)'''
+    
+'''for x in range(10):  #0 1 2 3 4 5 6 7 8 9         
+        if(x%2==0):
+           print(x)
+        
+print("\n")'''
+
+print("\n")
+for num in range(1500, 2700):
+    if(num%7==0 and num%5==0):
+        print(num)
