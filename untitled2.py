4cafc159bd23db593f0bfbe64985100f260f1bc0
maker565 <oceanoantarticoagua@gmail.com>
2026-02-24 13:57:35 -0300

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,14 @@
+'''numeros = [6,5,3,8,4,2,5,4,11]
+soma = 0
+
+for valor in numeros:
+    soma = soma + valor
+    
+print("A soma é", soma)'''
+
+frase = "meu programa em python"
+for letra in frase:
+    if(letra=="a" or letra=="e" or letra=="o"):
+        print("x")
+    else:
+         print(letra)   
