d508a283801c3417e8a83f48b3a589993712f246
maker565 <oceanoantarticoagua@gmail.com>
2026-02-25 13:07:47 -0300

    Add files via upload

---
+++ Teste1.py
@@ -0,0 +1,13 @@
+
+print("=======================")
+nome = input("Qual o seu nome? \n")
+idade = input("Qual a sua idade? \n")
+
+idade = int( idade )
+
+if idade >= 18:
+ print("Acesso permitido.")
+else:
+  print("Acesso negado.") 
+print("=======================")
+ 
